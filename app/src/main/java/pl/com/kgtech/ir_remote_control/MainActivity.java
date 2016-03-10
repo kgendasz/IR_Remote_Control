b4568f293f7798dee44df11548e5b2ffc660c7db
kgendasz <kgendasz@post.pl>
2016-03-10 23:34:46 +0100

    Pierwszy dzień

--- app/src/main/java/pl/com/kgtech/ir_remote_control/MainActivity.java
+++ app/src/main/java/pl/com/kgtech/ir_remote_control/MainActivity.java
@@ -1,13 +1,180 @@
 package pl.com.kgtech.ir_remote_control;
 
+import android.content.Context;
+import android.hardware.ConsumerIrManager;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ConsumerIrManager mCIR;
+    SparseArray<String> irData;
+    Button button1;
+    Button button2;
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
+        // Get a reference to the ConsumerIrManager
+        mCIR = (ConsumerIrManager) getSystemService(CONSUMER_IR_SERVICE);
+
+     //   irData.put(
+       //         R.id.button2,
+                hex2dec("0000 006d 0022 0003 00a9 00a8 0015 003f 0015 003f 0015 003f 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 003f 0015 003f 0015 003f 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 003f 0015 0015 0015 0015 0015 003f 0015 0015 0015 0015 0015 0015 0015 003f 0015 0015 0015 003f 0015 003f 0015 0015 0015 0040 0015 003f 0015 003f 0015 0702 00a9 00a8 0015 0015 0015 0e6e");
+
+        int i=3;
     }
+
+    public void onEndClick(View view) {
+
+        finish();
+    }
+
+    public void onButton1Click(View view) {
+        if (mCIR.hasIrEmitter() == false) {
+            Log.e("My app", "No IR Emitter found\n");
+
+        }
+        else
+            Log.e("My app", "IR Emitter found\n");
+    }
+
+    private static final int SAMSUNG_FREQ = 38028;
+    private static final int[] SAMSUNG_POWER_TOGGLE_COUNT = {169,168,21,63,21,63,21,63,21,21,21,21,21,21,21,21,21,21,21,63,21,63,21,63,21,21,21,21,21,21,21,21,21,21,21,21,21,63,21,21,21,21,21,21,21,21,21,21,21,21,21,64,21,21,21,63,21,63,21,63,21,63,21,63,21,63,21,1794,169,168,21,21,21,3694};
+
+    private static final int[] SAMSUNG_POWER_TOGGLE_DURATION = {4368,546,1638,546,1638,546,1638,546,546,546,546,546,546,546,546,546,546,546,1638,546,1638,546,1638,546,546,546,546,546,546,546,546,546,546,546,546,546,1638,546,546,546,546,546,546,546,546,546,546,546,546,546,1664,546,546,546,1638,546,1638,546,1638,546,1638,546,1638,546,1638,546,46644,4394,4368,546,546,546,96044};
+
+    public void onButton2Click(View view) {
+//        if (!mCIR.hasIrEmitter()) {
+//            Log.e("My app", "No IR Emitter found\n");
+//            return;
+//        }
+
+//        // Later version of Android 4.4.3
+//        mCIR.transmit(SAMSUNG_FREQ, SAMSUNG_POWER_TOGGLE_DURATION);
+
+
+
+        String data = hex2dec("0000 006d 0022 0003 00a9 00a8 0015 003f 0015 003f 0015 003f 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 003f 0015 003f 0015 003f 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 003f 0015 0015 0015 0015 0015 003f 0015 0015 0015 0015 0015 0015 0015 003f 0015 0015 0015 003f 0015 003f 0015 0015 0015 0040 0015 003f 0015 003f 0015 0702 00a9 00a8 0015 0015 0015 0e6e");
+
+        if (data != null) {
+            String values[] = data.split(",");
+            int[] pattern = new int[values.length - 1];
+
+            for (int i = 0; i < pattern.length; i++) {
+                pattern[i] = Integer.parseInt(values[i + 1]);
+            }
+
+            mCIR.transmit(Integer.parseInt(values[0]), pattern);
+            Log.e("My app", "Finish transmit\n");
+        }
+
+    }
+
+
+
+
+    protected String hex2dec(String irData) {
+        List<String> list = new ArrayList<String>(Arrays.asList(irData
+                .split(" ")));
+        list.remove(0); // dummy
+        int frequency = Integer.parseInt(list.remove(0), 16); // frequency
+        list.remove(0); // seq1
+        list.remove(0); // seq2
+
+        for (int i = 0; i < list.size(); i++) {
+            list.set(i, Integer.toString(Integer.parseInt(list.get(i), 16)));
+        }
+
+        frequency = (int) (1000000 / (frequency * 0.241246));
+        list.add(0, Integer.toString(frequency));
+
+        irData = "";
+        for (String s : list) {
+            irData += s + ",";
+        }
+        return irData;
+    }
+
+
+
+
+
+
+
+
+
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://pl.com.kgtech.ir_remote_control/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://pl.com.kgtech.ir_remote_control/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
+
+
+
 }
