e0232320064fb8101e5f9063911d47dfce4470a2
kgendasz <kgendasz@post.pl>
2016-03-10 19:29:31 +0100

    Initial commit

---
+++ app/src/main/java/pl/com/kgtech/ir_remote_control/MainActivity.java
@@ -0,0 +1,13 @@
+package pl.com.kgtech.ir_remote_control;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
